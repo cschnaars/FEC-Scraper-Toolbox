5ade137ed4c3e512271e56a77c7a3fce9d04fa60
Christopher Schnaars <cschnaars@gmail.com>
2015-05-30 08:42:54 -0400

    Tweaked get_delim_report_url; all related tests pass

--- fec_tools/lib/get_delim_report_url.py
+++ fec_tools/lib/get_delim_report_url.py
@@ -4,38 +4,38 @@ import requests
 from FEC_Toolbox.settings import FILE_TYPES
 
 
-def get_delim_report_url(rpt_id, filetype=None, url_patterns=None, delim=None, a_id=None):
+def get_delim_report_url(rpt_id, filetype=None, url_patterns=None, a_id=None):
     """
     Determine a valid URL that can be used to download the specified report (rpt_id) in the desired format (filetype).
-    If filetype is not a key in settings.FILE_TYPES, url_patterns and delim must be specified. Presently this function
-    works only for CSV and ASCII 28-delimited reports because URLs to view reports in PDF format or page-by-page in a
-    web browser are not available from the pages used below. This functionality needs to be added, probably as a
-    separate function.
+
+    The easiest way to use this function is to set filetype to a key in settings.FILE_TYPES. If this is done, values
+    for url_patterns and a_id will be retrieved from this dictionary. If filetype is not a key in settings.FILE_TYPES,
+    this parameter is ignored and either url_patterns or a_id must be set.
+
+    It is not possible to use URL patterns to retrieve an electronically filed report in CSV format when that report has
+    fewer than 1,000 rows. This is because the FEC website generates a random folder in the URL for these reports. To
+    get a valid URL, set the a_id parameter.
 
     :param rpt_id: Report ID. Required. This is the ID assigned by the FEC for a specific report. It is used as the
     primary key for reports in the django database.
     :type rpt_id: int
 
     :param filetype: File type. Key used to look up default values in settings.FILE_TYPES. The function does not
-    otherwise use or care about this variable. If this parameter is not provided, url_patterns, delim and i_id must be
-    passed to the function.
+    otherwise use or care about this variable. If this parameter is not provided, url_patterns or a_id must bepassed to
+    the function.
     :type filetype: str
 
     :param url_patterns: URL patterns. List of patterns used by the function to attempt to construct a valid URL to
-    download the specified report. You need to set this value only if filetype is not set or is not a key in
-    settings.FILE_TYPES or you want to override the default settings (the elec_url_pattern and paper_url_pattern
+    download the specified report. You need to set this value only if filetype and a_id are not set; filetype is not a
+    key in settings.FILE_TYPES; or you want to override the default settings (the elec_url_pattern and paper_url_pattern
     attributes) specified in settings.FILE_TYPES.
     :type url_patterns: list
 
-    :param delim: Delimiter. Delimiter used to parse reports downloaded in a text format, such as CSV or ASCII-28. You
-    need to set this value only if filetype is not set or is not a key in settings.FILE_TYPES or you want to override
-    the default setting (the delim attribute) specified in settings.FILE_TYPES.
-    :type delim: str
-
     :param a_id: <a> tag ID. Value of the id attribute of the <a> tag that houses a valid URL for the report. Generally
-    speaking, the <a> tag should be necessary only when you are trying to download an electronically filed report in
-    CSV format. You need to set this value only if filetype is not set or is not a key in settings.FILE_TYPES or you
-    want to override the default setting (the a_id attribute) specified for that key in settings.FILE_TYPES.
+    speaking, the <a> tag should be necessary only when you are trying to download an electronically filed report with
+    fewer than 1,000 rows in CSV format. You need to set this value only if filetype and url_patterns are not set;
+    filetype is not a key in settings.FILE_TYPES; or you want to override the default setting (the a_id attribute)
+    specified in settings.FILE_TYPES.
     :type a_id: str
 
     :return: str
@@ -51,6 +51,15 @@ def get_delim_report_url(rpt_id, filetype=None, url_patterns=None, delim=None, a
             if FILE_TYPES[filetype]['paper_url_pattern'] is not None:
                 url_patterns.append(FILE_TYPES[filetype]['paper_url_pattern'])
 
+    # If url_patterns is still empty and filetype not specified, try to fetch patterns based on a_id:
+    if url_patterns == [] and filetype is None and a_id is not None:
+        for key in FILE_TYPES.keys():
+            if FILE_TYPES[key]['a_id'] == a_id and FILE_TYPES[key]['delim'] is not None:
+                if FILE_TYPES[key]['elec_url_pattern'] is not None:
+                    url_patterns.append(FILE_TYPES[key]['elec_url_pattern'])
+                if FILE_TYPES[key]['paper_url_pattern'] is not None:
+                    url_patterns.append(FILE_TYPES[key]['paper_url_pattern'])
+
     # Iterate through patterns to construct a valid URL
     for url_pattern in url_patterns:
         url = url_pattern.replace('<rpt_id>', str(rpt_id))
@@ -58,16 +67,13 @@ def get_delim_report_url(rpt_id, filetype=None, url_patterns=None, delim=None, a
         if x.status_code == 200:
             return url
 
-    # If the function has not found a valid URL and a delimiter and <a> tag are available, attempt to scrape the URL.
-    if delim is None:
-        if filetype in FILE_TYPES.keys():
-            delim = FILE_TYPES[filetype]['delim']
-
+    # If the function has not found a valid URL and an <a> tag is available, attempt to scrape the URL.
+    # Note: This does not work for report of less than 1,000 rows filed on paper.
     if a_id is None:
         if filetype in FILE_TYPES.keys():
             a_id = FILE_TYPES[filetype]['a_id']
 
-    if delim is not None and a_id is not None:
+    if a_id is not None:
         base_url = 'http://docquery.fec.gov'
         url = base_url + '/cgi-bin/forms/DL/' + str(rpt_id)
         html = requests.get(url)
--- fec_tools/tests/test_get_delim_report_url.py
+++ fec_tools/tests/test_get_delim_report_url.py
@@ -5,63 +5,141 @@ import fec_tools.lib.get_delim_report_url
 
 # Tests for lib\get_report_url
 class TestGetReportUrl(unittest.TestCase):
-    def test_ascii28_defaults_elec(self):
+    """
+    Test reports:
+    Electronic report with less than 1,000 rows: 1008299
+    Electronic report with more than 1,000 rows: 877183
+    Paper report with less than 1,000 rows: 1006117
+    Paper report with more than 1,000 rows: 974135
+
+    It is not possible to use URL patterns to retrieve an electronically filed report in CSV format when that report has
+    fewer than 1,000 rows. This is because the FEC website generates a random folder in the URL for these reports. To
+    get a valid URL, set the a_id parameter.
+    """
+    def test_ascii28_elec_lt_1000_filetype(self):
         self.assertEqual('http://docquery.fec.gov/dcdev/posted/1008299.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, filetype='ascii28'))
 
-    def test_ascii28_defaults_paper(self):
+    def test_ascii28_elec_gt_1000_filetype(self):
+        self.assertEqual('http://docquery.fec.gov/dcdev/posted/877183.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, filetype='ascii28'))
+
+    def test_ascii28_paper_lt_1000_filetype(self):
         self.assertEqual('http://docquery.fec.gov/paper/posted/1006117.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, filetype='ascii28'))
 
-    def test_csv_defaults_elec_gt_1000_rows(self):
-        self.assertEqual('http://docquery.fec.gov/comma/877183.fec',
-                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, filetype='csv'))
+    def test_ascii28_paper_gt_1000_filetype(self):
+        self.assertEqual('http://docquery.fec.gov/paper/posted/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, filetype='ascii28'))
 
-    def test_csv_defaults_elec_lt_1000_rows(self):
+    def test_csv_elec_lt_1000_filetype(self):
         """
         The xxx in the expected URL is a randomly generated folder in the actual URL.
         """
         url = fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, filetype='csv')
-        url_mod = url[:url.find('/showcsv/') + 9] + 'xxx' + url[url.find('/showcsv/') + 16:]
+        # Replace random folder name with xxx
+        url_mod = url[:url.find('/showcsv/') + 9] + 'xxx' + url[len(url) - url[::-1].find('/') - 1:]
         self.assertEqual('http://docquery.fec.gov/showcsv/xxx/1008299.fec', url_mod)
 
-    def test_csv_defaults_paper(self):
+    def test_csv_elec_gt_1000_filetype(self):
+        self.assertEqual('http://docquery.fec.gov/comma/877183.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, filetype='csv'))
+
+    def test_csv_paper_lt_1000_filetype(self):
         self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/1006117.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, filetype='csv'))
 
-    def test_url_pattern_only_ascii28_elec(self):
+    def test_csv_paper_gt_1000_filetype(self):
+        self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, filetype='csv'))
+
+    def test_ascii28_elec_lt_1000_url_patterns(self):
         self.assertEqual('http://docquery.fec.gov/dcdev/posted/1008299.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, url_patterns=[
                              'http://docquery.fec.gov/dcdev/posted/<rpt_id>.fec',
                              'http://docquery.fec.gov/paper/posted/<rpt_id>.fec']))
 
-    def test_url_pattern_only_ascii28_paper(self):
+    def test_ascii28_elec_gt_1000_url_patterns(self):
+        self.assertEqual('http://docquery.fec.gov/dcdev/posted/877183.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, url_patterns=[
+                             'http://docquery.fec.gov/dcdev/posted/<rpt_id>.fec',
+                             'http://docquery.fec.gov/paper/posted/<rpt_id>.fec']))
+
+    def test_ascii28_paper_lt_1000_url_patterns(self):
         self.assertEqual('http://docquery.fec.gov/paper/posted/1006117.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, url_patterns=[
                              'http://docquery.fec.gov/dcdev/posted/<rpt_id>.fec',
                              'http://docquery.fec.gov/paper/posted/<rpt_id>.fec']))
 
-    def test_url_pattern_only_csv_elec_gt_1000_rows(self):
+    def test_ascii28_paper_gt_1000_url_patterns(self):
+        self.assertEqual('http://docquery.fec.gov/paper/posted/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, url_patterns=[
+                             'http://docquery.fec.gov/dcdev/posted/<rpt_id>.fec',
+                             'http://docquery.fec.gov/paper/posted/<rpt_id>.fec']))
+
+    def test_csv_elec_lt_1000_url_patterns(self):
+        """
+        It is not possible to use URL patterns to retrieve an electronically filed report in CSV format when that
+        report has fewer than 1,000 rows. This is because the FEC website generates a random folder in the URL for
+        these reports. To get a valid URL, set the a_id parameter.
+        """
+        pass
+
+    def test_csv_elec_gt_1000_url_patterns(self):
         self.assertEqual('http://docquery.fec.gov/comma/877183.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, url_patterns=[
                              'http://docquery.fec.gov/comma/<rpt_id>.fec',
                              'http://docquery.fec.gov/paper/fecpprcsv/<rpt_id>.fec']))
-        
-    def test_url_pattern_only_csv_elec_lt_1000_rows(self):
-        """
-        The xxx in the expected URL is a randomly generated folder in the actual URL.
-        """
-        url = fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, url_patterns=[
-                             'http://docquery.fec.gov/comma/<rpt_id>.fec',
-                             'http://docquery.fec.gov/paper/fecpprcsv/<rpt_id>.fec'])
-        url_mod = url[:url.find('/showcsv/') + 9] + 'xxx' + url[url.find('/showcsv/') + 16:]
-        self.assertEqual('http://docquery.fec.gov/showcsv/xxx/1008299.fec', url_mod)
 
-    def test_url_pattern_only_csv_paper(self):
+    def test_csv_paper_lt_1000_url_patterns(self):
         self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/1006117.fec',
                          fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, url_patterns=[
                              'http://docquery.fec.gov/comma/<rpt_id>.fec',
                              'http://docquery.fec.gov/paper/fecpprcsv/<rpt_id>.fec']))
+
+    def test_csv_paper_gt_1000_url_patterns(self):
+        self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, url_patterns=[
+                             'http://docquery.fec.gov/comma/<rpt_id>.fec',
+                             'http://docquery.fec.gov/paper/fecpprcsv/<rpt_id>.fec']))
+
+    def test_ascii28_elec_lt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/dcdev/posted/1008299.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, a_id='asciifile'))
+
+    def test_ascii28_elec_gt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/dcdev/posted/877183.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, a_id='asciifile'))
+
+    def test_ascii28_paper_lt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/paper/posted/1006117.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, a_id='asciifile'))
+
+    def test_ascii28_paper_gt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/paper/posted/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, a_id='asciifile'))
+
+    def test_csv_elec_lt_1000_a_id(self):
+        """
+        The xxx in the expected URL is a randomly generated folder in the actual URL.
+        """
+        url = fec_tools.lib.get_delim_report_url.get_delim_report_url(1008299, a_id='csvfile')
+        # Replace random folder name with xxx
+        url_mod = url[:url.find('/showcsv/') + 9] + 'xxx' + url[len(url) - url[::-1].find('/') - 1:]
+        self.assertEqual('http://docquery.fec.gov/showcsv/xxx/1008299.fec', url_mod)
+
+    def test_csv_elec_gt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/comma/877183.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(877183, a_id='csvfile'))
+
+    def test_csv_paper_lt_1000_a_id(self):
+        self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/1006117.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(1006117, a_id='csvfile'))
+
+    def test_csv_paper_gt_1000_a_id(self):
+        # Failed
+        self.assertEqual('http://docquery.fec.gov/paper/fecpprcsv/974135.fec',
+                         fec_tools.lib.get_delim_report_url.get_delim_report_url(974135, a_id='csvfile'))
 
 if __name__ == '__main__':
     unittest.main()

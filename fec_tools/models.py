6be3c2a5c721a432993c42398736d6bb52b80ad3
Christopher Schnaars <cschnaars@gmail.com>
2015-06-24 11:03:06 -0400

    Added very basic database models

--- fec_tools/models.py
+++ fec_tools/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ReportForm(models.Model):
+    rpt_id = models.PositiveIntegerField('Report ID', primary_key=True)
+    form = models.ForeignKey('Form', blank=False)
+
+
+class Form(models.Model):
+    form = models.CharField('Form', max_length=8, blank=False)
+
+
+class ReportCommittee(models.Model):
+    rpt_id = models.PositiveIntegerField('Report ID', primary_key=True)
+    committee = models.ForeignKey('Committee', blank=False)
+
+
+class Committee(models.Model):
+    fec_comm_id = models.CharField('FEC Committee ID', max_length=9)
